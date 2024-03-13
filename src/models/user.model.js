4f9ddf7bc4e6c8937bf0de804a4938f03b98bf49
Rani Saini <sainirani1797@gmail.com>
2024-03-13 11:46:33 +0530

    Code updated using Sequelize.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,78 +1,79 @@
+import sequelize from "sequelize"
+import { DataTypes } from "sequelize";
 
-const User = (sequelize, DataTypes) => {
-    const User = sequelize.define("User", {
-        firstName: {
-            type: DataTypes.STRING
-        },
+const User = sequelize.define("User", {
+    firstName: {
+        type: DataTypes.STRING
+    },
 
-        lastName: {
-            type: DataTypes.STRING
-        },
+    lastName: {
+        type: DataTypes.STRING
+    },
 
-        profilePhoto: {
-            type: DataTypes.STRING
-        },
+    profilePhoto: {
+        type: DataTypes.STRING
+    },
 
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
 
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
 
-        postCount: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0
-        },
+    postCount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
 
-        isBlocked: {
-            type: DataTypes.BOOLEAN
-        },
-        isAdmin: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
+    isBlocked: {
+        type: DataTypes.BOOLEAN
+    },
+    isAdmin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
 
-        role: {
-            type: DataTypes.ENUM('Admin', 'Guest', 'Editor')
+    role: {
+        type: DataTypes.ENUM('Admin', 'Guest', 'Editor')
 
-        },
+    },
 
-        active: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: true
-        }
-    });
+    active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    }
+});
 
 
-    // Associations
-    User.belongsToMany(User, {
-        through: 'UserViews',
-        as: 'viewers',
-        foreignKey: 'viewedUserId'
-    });
+// Associations
+User.belongsToMany(User, {
+    through: 'UserViews',
+    as: 'viewers',
+    foreignKey: 'viewedUserId'
+});
 
-    User.belongsToMany(User, {
-        through: 'UserFollowers',
-        as: 'following',
-        foreignKey: 'followerId',
-    });
+User.belongsToMany(User, {
+    through: 'UserFollowers',
+    as: 'following',
+    foreignKey: 'followerId',
+});
 
-    User.belongsToMany(User, {
-        through: 'UserFollows',
-        as: "followers",
-        foreignKey: 'followingId'
-    });
+User.belongsToMany(User, {
+    through: 'UserFollows',
+    as: "followers",
+    foreignKey: 'followingId'
+});
 
-    User.belongsToMany(Post, {
-        through: 'UserPosts',
-        as: 'posts'
-    });
+User.belongsToMany(Post, {
+    through: 'UserPosts',
+    as: 'posts'
+});
 
-}
+
 
 export default User;
